23bc00872ce8dda0b046b50b085bf8ba55d3c113
Floyd Kretschmar <floyd.kretschmar@student.uni-tuebingen.de>
2020-12-29 16:10:31 +0100

    Set up base folder for authentication

---
+++ auth/src/index.ts
@@ -0,0 +1,12 @@
+import express from "express";
+
+const app = express();
+app.use(express.json());
+
+app.get("/api/users/currentuser", (req, res) => {
+  res.send("Hello World!!");
+});
+
+app.listen(3000, () => {
+  console.log("Auth-Service listening on port 3000!");
+});
